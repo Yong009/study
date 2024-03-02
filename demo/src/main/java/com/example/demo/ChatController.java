d3baf8d130d65b913694cc23b0a8b49a95aac9fe
yong009 <rndyddjr09@naver.com>
2024-03-03 00:29:15 +0900

    로그인

--- demo/src/main/java/com/example/demo/ChatController.java
+++ demo/src/main/java/com/example/demo/ChatController.java
@@ -1,43 +1,16 @@
 package com.example.demo;
 
-import java.util.List;
-
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.demo.dto.ChatRoom;
-import com.example.demo.service.ChatService;
-
-import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
+@Log4j2
 public class ChatController {
-	private final ChatService chatService;
-	
-	@RequestMapping("/chat/chatList")
-    public String chatList(Model model){
-        List<ChatRoom> roomList = chatService.findAllRoom();
-        model.addAttribute("roomList",roomList);
-        return "chat/chatList";
+
+    @GetMapping("/chat")
+    public String chatGet(){
+        log.info("@ChatController, chat GET()");
+        return "chater";
     }
-	
-	@PostMapping("/chat/createRoom")  //방을 만들었으면 해당 방으로 가야지.
-    public String createRoom(Model model, @RequestParam String name, String username) {
-        ChatRoom room = chatService.createRoom(name);
-        model.addAttribute("room",room);
-        model.addAttribute("username",username);
-        return "chat/chatRoom";  //만든사람이 채팅방 1빠로 들어가게 됩니다
-    }
-	
-	 @GetMapping("/chat/chatRoom")
-	    public String chatRoom(Model model, @RequestParam String roomId){
-	        ChatRoom room = chatService.findRoomById(roomId);
-	        model.addAttribute("room",room);   //현재 방에 들어오기위해서 필요한데...... 접속자 수 등등은 실시간으로 보여줘야 돼서 여기서는 못함
-	        return "chat/chatRoom";
-	    }
 }
